f48768438ec77c46f97bf0af0935cf6b02b7ad05
aks861999 <49332175+aks861999@users.noreply.github.com>
2020-05-22 19:53:49 +0530

    use this code to convert the text into audio file

---
+++ text to speech.py
@@ -0,0 +1,17 @@
+#Import Google Text to Speech
+from gtts import gTTS 
+
+#Import Audio method from IPython's Display Class
+from IPython.display import Audio 
+
+#import the extracted text in read mode
+f = open("extracted.txt", "r")
+text=f.read()
+
+#Provide the string to convert to speech
+tts = gTTS(text ) 
+
+#save the string converted to speech as a .wav file
+tts.save('output.wav') 
+sound_file ='outputm.wav'
+Audio(sound_file, autoplay=True) 
