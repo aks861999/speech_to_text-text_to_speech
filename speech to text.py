ba7a92afafdfca5de29d578e00d26525d803794b
aks861999 <49332175+aks861999@users.noreply.github.com>
2020-05-22 19:29:31 +0530

    Update speech to text.py

--- speech to text.py
+++ speech to text.py
@@ -1,7 +1,21 @@
+### An open source contribution by Akash ###
+
+#import the module speech_recognition
 import speech_recognition as sr
 r = sr.Recognizer()
 sound_file = 'path to the audio file in .wav format'
 file_audio = sr.AudioFile(sound_file)
+
+# set the newly read audio file as the source to recognise as speech object
 with file_audio as source:
     audio_text = r.record(source)
+
+#save the recognised text in an object called 'text' for further processing
+text= r.recognize_google(audio_text)
+
 print(r.recognize_google(audio_text))
+
+# Now create a text file for further processing
+f = open("extracted.txt", "w")
+f.write(text)
+f.close()
