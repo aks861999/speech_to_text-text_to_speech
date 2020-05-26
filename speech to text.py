ce3b413212dae11434e4f6f89d77efa93f8d2b7b
aks861999 <biswas.ak.1999@gmail.com>
2020-05-26 15:16:22 +0530

    Use this code to convert Speech to Text

--- speech to text.py
+++ speech to text.py
@@ -5,17 +5,28 @@ import speech_recognition as sr
 r = sr.Recognizer()
 sound_file = 'path to the audio file in .wav format'
 file_audio = sr.AudioFile(sound_file)
+        
+try:
+    # set the newly read audio file as the source to recognise as speech object
+    with file_audio as source2: 
+        # wait for a second to let the recognizer adjust the energy threshold based on the surrounding noise level  
+        r.adjust_for_ambient_noise(source2, duration=0.5) 
 
-# set the newly read audio file as the source to recognise as speech object
-with file_audio as source:
-    audio_text = r.record(source)
+        #record the user's input  
+        audio2 = r.record(source2) 
 
-#save the recognised text in an object called 'text' for further processing
-text= r.recognize_google(audio_text)
+        #save the recognised text in an object called 'text' for further processing
+        text = r.recognize_google(audio2,key=None, show_all = False) 
+        
+        # Now create a text file for further processing
+        f = open("extracted.txt", "w")
+        f.write(text)
+        f.close()
 
-print(r.recognize_google(audio_text))
-
-# Now create a text file for further processing
-f = open("extracted.txt", "w")
-f.write(text)
-f.close()
+# what if Error occur in Requesting the Endpoint
+except sr.RequestError as e: 
+        print("Could not request results; {0}".format(e)) 
+          
+# what if the API get Unknown Value Returned from Endpoint
+except sr.UnknownValueError: 
+        print("unknown error occured") 
